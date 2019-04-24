8f68c70d77a8c804b1fb073ef54ff5300461648a
Caio Rodrigues <caiorodrigues@hotmail.com>
2019-04-24 02:48:34 -0300

    Commit Curso JS - ES6+ - Modulo Webpack Server

---
+++ js_es6/webpack-server/main.js
@@ -0,0 +1,17 @@
+//import { soma, sub } from "./funcoes";
+//import { soma as somaFunction, sub } from "./funcoes";
+//import soma, { sub } from "./funcoes";
+
+//import * as funcoes from "./funcoes";
+
+//import soma from "./soma";
+
+//console.log(soma(1, 2));
+//console.log(somaFunction(1, 2));
+
+//console.log(sub(4, 2));
+
+//console.log(funcoes);
+//console.log(funcoes.soma);
+//console.log(funcoes.sub);
+//console.log(funcoes.mult);
---
+++ js_es6/webpack-server/src/main.js
@@ -0,0 +1,17 @@
+//Webpack Server - AULA
+alert("Testando");
+
+/****************************************************************/
+//Webpack Server - Desafio 1.1
+//import ClasseUsuario from ".desafio2/exercicio1/functions";
+//ClasseUsuario.info();
+
+//Webpack Server - Desafio 1.2
+//import { idade } from "./desafio2/exercicio1/functions";
+//console.log(idade);
+
+//Webpack Server - Desafio 1.3
+//import Usuario, {
+//  idade as IdadeUsuario
+//} from "./desafio2/exercicio1/functions";
+/****************************************************************/
